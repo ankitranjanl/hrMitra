29089c928938df6d89b00ced47eb549a2840f795
Ankit Ranjan <ankitranjanlaha22@gmail.com>
2024-02-05 16:12:07 +0530

    link handler and partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  get '/home/index' ,to: 'home#index'
+  get '/about' ,to: 'pages#about_us'
+  get '/contact' ,to: 'pages#contact_us'
+  get '/privacy' ,to: 'pages#privacy_policy'
+  get '/terms_and_conditions' ,to: 'pages#terms_and_conditions'
+  # get '/home/index' ,to: 'home#index'
+  root "home#index"
 end
